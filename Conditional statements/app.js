6da5bbbbb3d5ced3227afb2ee1fb95459256ebbb
nikhilkdevx <nikhilmasti4@gmail.com>
2026-03-12 21:15:57 +0530

    learned switches

--- Conditional statements/app.js
+++ Conditional statements/app.js
@@ -1,8 +1,15 @@
-let Str = "apple";
-
-if ( Str.length > 3 && Str[0] === "a"){
-    console.log(" It is a good string");
-}
-else{
-    console.log(" It is not a good string");
+let color = "green";
+switch (color) 
+{
+        case "red":
+        console.log ("ruk ja Bc");
+        break;
+        case "yellow":
+        console.log ("e gaadi start kr");
+        break;
+        case "green":
+        console.log ("chal ja Bc");
+        break;
+        default:
+        console.log ("signal kharab hai");
 }
