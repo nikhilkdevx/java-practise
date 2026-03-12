98f0e4470404e71735afd76a42c2a4300abc9556
nikhilkdevx <nikhilmasti4@gmail.com>
2026-03-12 20:51:20 +0530

    deleted

--- Conditional statements/app.js
+++ Conditional statements/app.js
@@ -1,12 +1,7 @@
-let Size = "s";
-if (Size === "xl"){
-    console.log("price of popcorn is 250");
-
-} else if ( Size === "l"){
-    console.log("price of popcorn is 200");
-
-} else if ( Size === "m"){
-    console.log("price of popcorn is 150");
-} else if ( Size === "s"){
-    console.log("price of popcorn is 100");
-}
+let Size = "6";
+let letter = "b";
+if (Size > "3" && letter >= "a") {
+    console.log(" it is a good string");}
+    else if ( Size < "3" && letter >= "a") {
+        console.log(" it is not a good string");
+    }
