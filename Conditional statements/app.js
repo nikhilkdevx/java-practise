ea4b356c8a55aa9d404d7133fea03d00044f870a
nikhilkdevx <nikhilmasti4@gmail.com>
2026-03-12 16:49:54 +0530

    if and else if practice

--- Conditional statements/app.js
+++ Conditional statements/app.js
@@ -1,11 +1,12 @@
-let color = "green"
-if (color === "red") {
-    console.log(" Don't go, the light is red");
-};
-if(color === "green"){
-    console.log(" go , the light is green");
+let Size = "s";
+if (Size === "xl"){
+    console.log("price of popcorn is 250");
 
-}
-if (color === "yellow"){
-    console.log(" start your vehicle");
+} else if ( Size === "l"){
+    console.log("price of popcorn is 200");
+
+} else if ( Size === "m"){
+    console.log("price of popcorn is 150");
+} else if ( Size === "s"){
+    console.log("price of popcorn is 100");
 }
