d7e558295bcf153ba89bc59837e6e58fc14065d7
nikhilkdevx <nikhilmasti4@gmail.com>
2026-03-13 16:19:14 +0530

    solving practise question

--- Conditional statements/app.js
+++ Conditional statements/app.js
@@ -1,27 +1,7 @@
-let day = "1";
-switch (day) 
-{
-        case "1":
-        console.log ("aaj monday hai");
-        break;
-        case "2":
-        console.log ("aaj tuesday hai");
-        break;
-        case "3":
-        console.log ("aaj wednesday hai");
-        break;
-        case "4":
-        console.log ("aaj thursday hai");
-        break;
-        case "5":
-        console.log ("aaj friday hai");
-        break;
-        case "6":
-        console.log ("aaj saturday hai");
-        break;
-        case "7":
-        console.log ("aaj sunday hai");
-        break;
-        default:
-        console.log ("aaj koi din nahi hai");
+let num = 35;
+if (num % 10 === 0) {
+console.log("good");
+}
+else {
+console.log("bad");
 }
