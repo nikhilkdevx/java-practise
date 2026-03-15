858a4b560a52895ae73c8d6c7e4416c98a1a9f2e
nikhilkdevx <nikhilmasti4@gmail.com>
2026-03-15 16:24:16 +0530

    learned string methods

--- Conditional statements/app.js
+++ Conditional statements/app.js
@@ -1,8 +1,4 @@
-let a = 13;
-let b = 1234562;
-
-if (a % 10 == b % 10){
-        console.log("last digit of a and b are same");
-} else{
-        console.log("last digit of a and b are different");
-}
+let msg ="Nikhil will Achieve Freedom"
+let Newmsg = msg.repeat(3);
+console.log(Newmsg);
+ 
