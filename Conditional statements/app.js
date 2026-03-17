dcd500f2d7602010000ad916bec1051d7cdeed09
nikhilkdevx <nikhilmasti4@gmail.com>
2026-03-17 16:26:57 +0530

    learning indexof

--- Conditional statements/app.js
+++ Conditional statements/app.js
@@ -1,3 +1,8 @@
 let months = ["january","july","march","august"];
 let a = months.indexOf("march");
 console.log(a);
+let b = months.includes("august");
+console.log(b);
+let c =["1","2","3","4"];
+let d =months.concat(c);
+console.log(d);
