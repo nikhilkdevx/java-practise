6b53a785c11320b6658f450e94f8a29989ea89aa
nikhilkdevx <nikhilmasti4@gmail.com>
2026-03-18 16:51:58 +0530

    solving practice q

--- Conditional statements/app.js
+++ Conditional statements/app.js
@@ -1,6 +1,6 @@
-let arr = [7,9,0,-2];
-let n = 3;
-let ans = arr.slice(arr.length-n);
-console.log(ans);
-let b = arr.length();
-console.log(b);
+let str= prompt("please enter a string ");
+if (str.length == 0 ) {
+    console.log("string is empty");
+} else {
+    console.log ("string is not empty");
+} 
