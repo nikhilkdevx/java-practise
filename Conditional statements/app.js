ddc88a11b39118cd72a1a9bdc10520b3659e2476
nikhilkdevx <nikhilmasti4@gmail.com>
2026-03-18 16:19:04 +0530

    solving practice q

--- Conditional statements/app.js
+++ Conditional statements/app.js
@@ -1,4 +1,4 @@
-let arr = [["x"," ","o"],[" ","x"," "],["o"," ","x"]];
-console.log(arr);
- arr[0][1] = "o";
- console.log(arr);
+let arr = [7,9,0,-2];
+let n = 3;
+let ans = arr.slice(0,n);
+console.log(ans);
