83469d8d14eec8d701047bf680ad30d488808def
nikhilkdevx <nikhilmasti4@gmail.com>
2026-03-20 16:21:46 +0530

    learning loops

--- Loops/app.js
+++ Loops/app.js
@@ -6,6 +6,9 @@
 // for (let i = 15 ; i>=1; i = i -2){
 //     console.log(i);
 // }
-for (let i = 10 ; i >= 2 ; i = i-2){
+// for (let i = 10 ; i >= 2 ; i = i-2){
+//     console.log(i);
+// }
+for (let i = 5; i <= 100; i = i+5){
     console.log(i);
 }
