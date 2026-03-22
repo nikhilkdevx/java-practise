a1bb1157ed007511243021f0c258b0dd3a8c25d1
nikhilkdevx <nikhilmasti4@gmail.com>
2026-03-22 16:17:28 +0530

    everything oky

--- Loops/app.js
+++ Loops/app.js
@@ -1,5 +1,10 @@
-let i = 1;
-while (i <= 5){
-    console.log(i);
-    i++;
+const favMovie = "Krish";
+let guess = prompt("guess the movie");
+while ((guess != favMovie) && (guess != "quit")) {
+    guess = prompt("wrong,Try Again");
+}
+if ( favMovie === guess){
+    console.log("congrats you are right");}
+    else if("quit"=== guess) {
+    console.log("You Quit");
 }
