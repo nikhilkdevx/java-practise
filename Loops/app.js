ca5666bf28144fdf28fb518cd73d589fdd1916da
nikhilkdevx <nikhilmasti4@gmail.com>
2026-03-22 13:10:29 +0530

    everything oky

--- Loops/app.js
+++ Loops/app.js
@@ -1,16 +1,5 @@
-// for(let i= 1; i <= 15; i = i+2){
-//     console.log(i);
-// }
-// // let a = ;
-// console.log("backwards");
-// for (let i = 15 ; i>=1; i = i -2){
-//     console.log(i);
-// }
-// for (let i = 10 ; i >= 2 ; i = i-2){
-//     console.log(i);
-// }
-let n = prompt("enter your number");
-n = parseInt(n);
-for( let i = n ; i <= n*10; i=i+n){
+let i = 1;
+while (i <= 5){
     console.log(i);
+    i++;
 }
