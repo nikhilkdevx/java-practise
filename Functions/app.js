ffc52a521ca8cc8854fe9296e0db9bad8b9daa32
nikhilkdevx <nikhilmasti4@gmail.com>
2026-04-01 17:40:41 +0530

    functions

--- Functions/app.js
+++ Functions/app.js
@@ -1,5 +1,20 @@
-function twninkle(){
-  console.log("Twinkle, twinkle, little star How I wonder what you are Up above the world so high Like a diamond in the sky");
+// function Name(name , age) {
+//     // console.log(`name is ${name} and age is ${age}`);
+//     console.log(name, age);
+// }
+// Name("Nik",20);
 
+function Mul(i){
+    console.log(i*1);
+    console.log(i*2);
+    console.log(i*3);
+    console.log(i*4);
+    console.log(i*5);
+    console.log(i*6);
+    console.log(i*7);
+    console.log(i*8);
+    console.log(i*9);
+    console.log(i*10);
+    
 }
-twninkle();
+Mul(65);
