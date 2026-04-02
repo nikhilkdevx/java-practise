f166190c475f76cc2ed00082d88d36c573f4cf6f
nikhilkdevx <nikhilmasti4@gmail.com>
2026-04-02 16:53:29 +0530

    function practicing

--- Functions/app.js
+++ Functions/app.js
@@ -1,20 +1,40 @@
-// function Name(name , age) {
-//     // console.log(`name is ${name} and age is ${age}`);
-//     console.log(name, age);
-// }
-// Name("Nik",20);
+// // function Name(name , age) {
+// //     // console.log(`name is ${name} and age is ${age}`);
+// //     console.log(name, age);
+// // }
+// // Name("Nik",20);
 
-function Mul(i){
-    console.log(i*1);
-    console.log(i*2);
-    console.log(i*3);
-    console.log(i*4);
-    console.log(i*5);
-    console.log(i*6);
-    console.log(i*7);
-    console.log(i*8);
-    console.log(i*9);
-    console.log(i*10);
+// function Mul(i){
+//     console.log(i*1);
+//     console.log(i*2);
+//     console.log(i*3);
+//     console.log(i*4);
+//     console.log(i*5);
+//     console.log(i*6);
+//     console.log(i*7);
+//     console.log(i*8);
+//     console.log(i*9);
+//     console.log(i*10);
     
+// }
+// Mul(65); 
+
+// function sumofn(n){
+//     let sum =0;
+//     for(let i=1;i<=n;i++){
+//         sum = sum+i
+        
+//     }
+//     return sum;
+// }
+
+// // sumofn(6);
+// sumofn(7);
+    let arr = ["Nik","is","coding"];
+    function add(arr){
+    let result="";
+    for(let i=0;i<arr.length;i++){
+        result += arr[i];
+    }
+    return result;
 }
-Mul(65);
