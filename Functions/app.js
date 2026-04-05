e55fd32549823d096a02304a9b6ad0cb4bd3dc75
nikhilkdevx <nikhilmasti4@gmail.com>
2026-04-05 16:30:48 +0530

    doing practice for functions

--- Functions/app.js
+++ Functions/app.js
@@ -28,13 +28,38 @@
 //     return sum;
 // }
 
-// // sumofn(6);
-// sumofn(7);
-    let arr = ["Nik","is","coding"];
-    function add(arr){
-    let result="";
-    for(let i=0;i<arr.length;i++){
-        result += arr[i];
+// // // sumofn(6);
+// // sumofn(7);
+//     let arr = ["Nik","is","coding"];
+//     function add(arr){
+//     let result="";
+//     for(let i=0;i<arr.length;i++){
+//         result += arr[i];
+//     }
+//     return result;
+// }  
+// let arr =[2,3,4,5,6,9,10];
+// let num = 8;
+// let result =[];
+// for (let i = 0;i < arr.length; i++){
+//     if(arr[i]>num){
+//         result.push(arr[i]);
+//     }
+// }
+// console.log(result);
+// let str ="aabbccddeeffgghtt";
+// let resultstr = "";
+// for (let i=0; i<str.length ; i++){
+//     if(!resultstr.includes(str[i])){
+//         resultstr += str[i];
+//     }
+// }
+// console.log(resultstr);
+let country =["Hi", "No", "Yes"];
+let largestcountry ="";
+for (let i=0;i<country.length;i++){
+    if(country[i].length>largestcountry.length){
+        largestcountry = country[i] ;
     }
-    return result;
 }
+console.log(largestcountry);
